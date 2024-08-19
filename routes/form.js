a30f057c64e502d38c167ecf03f26277bfa34df2
carmelshavit <carmel.shavit@gmail.com>
2024-08-19 17:41:15 +0300

    add deploy

--- routes/form.js
+++ routes/form.js
@@ -10,12 +10,22 @@ const PORT = process.env.PORT || 3001;
 // Helper function to fetch metadata
 const fetchMetadata = async (url) => {
 	try {
+		console.log(`Fetching metadata for URL: ${url}`);
+
 		const { data } = await axios.get(url);
+		console.log(`Data fetched successfully for URL: ${url}`);
+
 		const $ = cheerio.load(data);
 
 		const title = $('head title').text() || null;
 		const description = $('meta[name="description"]').attr('content') || null;
 		const image = $('meta[property="og:image"]').attr('content') || null;
+
+		console.log(`Extracted metadata for URL: ${url}`, {
+			title,
+			description,
+			image,
+		});
 
 		return { title, description, image };
 	} catch (error) {
@@ -27,23 +37,24 @@ const fetchMetadata = async (url) => {
 // API endpoint to get metadata for a URL
 router.get('/metadata', async (req, res) => {
 	const { url } = req.query;
+	console.log('server 40:', req);
 
 	if (!url) {
+		console.error('No URL provided in request');
 		return res.status(400).json({ error: 'URL is required' });
 	}
 
 	try {
+		console.log(`Received request to fetch metadata for URL: ${url}`);
+
 		const metadata = await fetchMetadata(url);
+		console.log(`Sending metadata response for URL: ${url}`, metadata);
+
 		res.json(metadata);
 	} catch (error) {
+		console.error(`Error occurred while fetching metadata for URL: ${url}`);
 		res.status(500).json({ error: 'Failed to fetch metadata' });
 	}
 });
-
-// const url = 'https://www.facebook.com';
-// (async () => {
-// 	const res = await fetchMetadata(url);
-// 	console.log(res);
-// })();
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,20 +1,29 @@
 /** @format */
 
 const express = require('express');
+const rateLimitMiddleware = require('./rateLimiter');
+
 const app = express();
 const cors = require('cors');
 const apiRoute = require('./routes/form');
 
-app.use(
-	cors({
-		origin: 'http://localhost:5173',
-		methods: ['GET', 'POST', 'PUT', 'DELETE'],
-		credentials: true,
-	})
-);
-app.use(express.json());
 
-app.use('/api', apiRoute);
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.resolve('build')));
+} else {
+	app.use(
+		cors({
+			origin: 'http://localhost:5173',
+			methods: ['GET', 'POST', 'PUT', 'DELETE'],
+			credentials: true,
+		})
+	);
+}
+
+app.use(express.json());
+app.use(rateLimitMiddleware);
+
+app.use('/api', rateLimitMiddleware, apiRoute);
 
 const port = 3001;
 
